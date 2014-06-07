ba4bc8941be5b770972aac8ad1969b027aef3cf6
zigvt85 <zigvt85@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-07 19:17:19 +0000

    Changed back to the old widget so you do not have that annoying error log. Will fix in a later date!
    git-svn-id: https://plugins.svn.wordpress.org/random-youtube-video/trunk@928275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- youtubevideo.php
+++ youtubevideo.php
@@ -1,258 +1,123 @@
 <?php
-
 /*
-
 Plugin Name: Random YouTube Video
-
 Plugin URI: http://wordpress.org/plugins/random-youtube-video/
-
 Description: This widget shows a random youtube video from your video list in your wordpress sidebar
-
-Current Author: http://www.soslidesigns.com
-
+Download URL: http://www.soslidesigns.com/files/random-youtube-videos.zip
 Author: Shobba, roycegracie, zigvt85
-
 Author Notes: Community patch by the authors above fixed from using object to iframe and menu glitch where
-
 the menu was going under the video if close enough.
-
 Author URI: http://wordpress.org/plugins/random-youtube-video/
-
 Version: 1.8
-
 License: GPL compatible
-
 */
-
-
 
 /*  Copyright 2008  Marcel Eichhorn
 
-
-
     This program is free software; you can redistribute it and/or modify
-
     it under the terms of the GNU General Public License as published by
-
     the Free Software Foundation; either version 2 of the License, or
-
     (at your option) any later version.
 
-
-
     This program is distributed in the hope that it will be useful,
-
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
     GNU General Public License for more details.
 
-
-
     For a copy of the GNU General Public License see <http://www.gnu.org/licenses/>.
-
 */
 
-
-
 function addrowfunc()
-
 {
-
     $url = get_bloginfo('wpurl');
-
     echo "\n\t<!-- Added By Random YouTube Video -->\n\t";
-
     ?>
-
 	<script language="JavaScript">
-
 		function newrow(last, obj){
-
 			var latestid = last;
-
 			tb = document.getElementById("asd");
-
 			var inn2 = '<table width="100%"><tr><th><input type="Text" name="titel['+latestid+']" value="" size="50"></th><th><input type="Text" name="url['+latestid+']" value="" size="50"></th></tr></table>';
-
 			tb.innerHTML += inn2;
-
 			/*
-
 			tb = document.getElementById("greattable");
-
 			tr = document.createElement("tr");
-
 			th1 = document.createElement("th");
-
 			th2 = document.createElement("th");
 
-
-
 			tb.appendChild(tr);
-
 			tr.appendChild(th1);
-
 			tr.appendChild(th2);
 
-
-
 			th1.innerHTML = '<input type="Text" name="titel['+latestid+']" value="" size="50">';
-
 			th2.innerHTML = '<input type="Text" name="url['+latestid+']" value="" size="50">';*/
-
 			latestid += 1;
-
 			obj.onclick = function(){newrow(latestid, obj);};
 
-
-
 			return false;
-
 		}
-
 	</script>
-
     <?
-
 //    echo "\n\t<script language='text/javascript' src='".$url."/wp-content/plugins/randomyoutubevideo/addrow.js'></script>";
-
 }
-
 function ryv_adminpage(){
-
 	global $wpdb;
-
 	if($_POST['ryv_submit']){
-
 	$leeren = $wpdb->query("TRUNCATE TABLE `".$wpdb->prefix."randomyoutube`");
-
 		if($_POST['url']){
-
 			foreach($_POST['url'] as $key => $con){
-
 				if($con){
-
 					$ins = $wpdb->query("INSERT INTO `".$wpdb->prefix."randomyoutube` (`id`,`url`,`titel`) VALUES (".$key.",'".$con."','".$_POST['titel'][$key]."')");
-
 				}
-
 			}
-
 			$message = '<div class="updated"><p><strong>Saved.</strong></p></div>';
-
 		}
-
 	}?>
-
 	<div class="wrap">
-
 		<?=$message?>
-
 		<h2>Random YouTube Video Management</h2>
-
 		<form name="ryv" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
-
 			<fieldset class="options">
-
 				<legend>YouTube Video List</legend>
-
 				<center>
-
 				To delete one entry just delete its url and click "Save"
-
 				<table width="80%" cellspacing="2" cellpadding="3" class="editform" id="greattable">
-
 					<tr style="background-color:#464646;color:white;">
-
 						<th style="text-align: center;">Video Title (Optional)</th>
-
 						<th style="text-align: center;">Video ID (From Embed Code)<br>(looks like: j9c5N2HzaHY)</th>
-
 					</tr>
-
 					<?
-
 					$vids = $wpdb->get_results("SELECT * FROM `".$wpdb->prefix."randomyoutube` ORDER BY id");
-
 					foreach ($vids as $vid){
-
 						$letzte_id = $vid->id;
-
 						?>
-
 					<tr>
-
 						<th style="text-align: center;"><input type="Text" name="titel[<?=$vid->id?>]" value="<?=$vid->titel?>" size="50"></th>
-
 						<th style="text-align: center;"><input type="Text" name="url[<?=$vid->id?>]" value="<?=$vid->url?>" size="50"></th>
-
 					</tr>
-
 					<?
-
 					}
-
 					?>
-
 				</table><table width="80%" cellspacing="2" cellpadding="3" class="editform"><tr><td id="asd"></td></tr></table>
-
 				<table width="80%" cellspacing="2" cellpadding="3" class="editform">
-
 					<tr>
-
 						<th colspan="2" style="text-align: left;"><input class="button" type="button" name="addrow_button" value="+ add row" onClick="return newrow(<?=$letzte_id+1?>, this);"></th>
-
 					</tr>
-
 				</table><input class="button" type="Submit" name="ryv_submit" value="Save"></center>
-
 			</fieldset>
-
 		</form>
-
 	</div>
-
 	<?
-
 }
-
-
 
 function ryv_adminmenu() {
-
     add_submenu_page('options-general.php', 'Random YouTube Video &raquo; Manage Videos', 'Random Youtube Videos', 10, __FILE__, 'ryv_adminpage');
-
 }
-
 add_action('admin_menu', 'ryv_adminmenu');
 
-/**
- * Adds ryv_Widget widget.
- */
-class ryv_Widget extends WP_Widget {
-
-	/**
-	 * Register widget with WordPress.
-	 */
-	function __construct() {
-		parent::__construct(
-			'ryv_widget', // Base ID
-			__('Random YT Video', 'text_domain'), // Name
-			array( 'description' => __( 'Random YT Video', 'text_domain' ), ) // Args
-		);
-	}
-
-	/**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
-    function widget($args, $instance) {
+function ryv_widget(){
+	if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') )
+		return;
+	function ryv_mywidget($args) {
 		global $wpdb;
 
 		extract($args);
@@ -269,23 +134,17 @@ class ryv_Widget extends WP_Widget {
 		else{
 		$video = $wpdb->get_results("SELECT * FROM `".$wpdb->prefix."randomyoutube` ORDER BY RAND() LIMIT 1");
 		$url=$video[0]->url; $name=$video[0]->titel;
-		
-            $widget = "<iframe width='$width' height='$height' src='//www.youtube.com/embed/$url?$autoplay' frameborder='0' allowfullscreen></iframe>";
-            echo $before_widget;
-            echo $before_title . $title . $after_title;
-            echo $widget;
-            echo $after_widget;
-        }
-    }
+		?>
+		<div align="left">
+			<font style="font-size:10px;"><? echo $name; ?><br /></font>
+			<iframe width="<?=$width?>" height="<?=$height?>" src="//www.youtube.com/embed/<? echo $url; ?>?<?=$autoplay?>" frameborder="0" allowfullscreen></iframe>
+			<? if($options['link']==1){?><br /><a href="http://wordpress.org/plugins/random-youtube-video/" target="_blank">RYV plugin by Shobba</a><?}?>
+		</div>
 
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
-	function form( $instance ) {
+<?		}
+		echo $after_widget;
+	}
+	function ryv_mywidget_control() {
 		$options = $newoptions = get_option('ryv_mywidget');
 		if ( $_POST['ryv_submit'] ) {
 			$newoptions['title'] = strip_tags(stripslashes($_POST['ryv_title']));
@@ -320,59 +179,38 @@ class ryv_Widget extends WP_Widget {
 				<input style="width: 50px;" id="ryv_width" name="ryv_width" type="text" value="<?=$options['width']; ?>" /> ( px )
 			</label>
 		</p>		
+		<p>
+			<label for="ryv_link">
+				<input type="Checkbox" name="ryv_link" id="ryv_link" value="1" <? if($options['link']==1){echo "checked";}?>> Show link to plugin?
+			</label>
+		</p>
 		<p style="text-align:right;margin-right:40px;"><?php echo $error; ?></p>
 		<input type="hidden" id="ryv_submit" name="ryv_submit" value="1" />
-		<?php 
+		<?
 	}
-
-} // class ryv_Widget
-
-// register ryv_Widget widget
-function register_ryv_widget() {
-    register_widget( 'ryv_Widget' );
+	register_sidebar_widget('Random YT Video', 'ryv_mywidget');
+	register_widget_control('Random YT Video', 'ryv_mywidget_control');
 }
-add_action( 'widgets_init', 'register_ryv_widget' );
-
 function ryv_install(){
-
 	global $wpdb;
 
 	$table_name = $wpdb->prefix . "randomyoutube";
-
 	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name){
-
 	$sql = "CREATE TABLE " . $table_name . " (
-
 	  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-
 	  url TEXT NOT NULL,
-
-	  title TEXT NOT NULL,
-
+	  titel TEXT NOT NULL,
 	  autoplay TEXT NOT NULL,
-
 	  UNIQUE KEY id (id)
-
 	);";
 
-
-
 	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
 	dbDelta($sql);
-
 	}
-
 }
 
-
-
 add_action('plugins_loaded', 'ryv_widget');
-
 register_activation_hook(__FILE__,'ryv_install');
-
 //if ($_REQUEST['page'] == "youtubevideo.php")
-
     add_action('admin_head', 'addrowfunc');
-
 ?>
