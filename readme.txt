ac2f50ed1886481ab800b239867290c399a4db0d
zigvt85 <zigvt85@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-27 13:54:30 +0000

    Updated all files for 2.1 version information is in the readme.txt file!
    git-svn-id: https://plugins.svn.wordpress.org/random-youtube-video/tags/2.1@997640 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Random YouTube Video ===
+Contributors: Shobba, zigvt85, roycegracie
+Tags: youtube, random, video, widget, sidebar
+Requires at least: 2.3
+Tested up to: 4.0
+Stable tag: trunk
+
+This sidebar widget shows a random youtube video from your own list.
+
+== Description ==
+
+This plugin adds a sidebar widget to your wordpress, which shows a random youtube video with own title from a list which you can manage in the admin area.
+
+I hope you'll enjoy it, feel free to leave a comment :-)
+
+<strong>Demo:</strong> http://www.soslidesigns.com/demo
+
+<strong>Tested with Windows and Linux and with the latest wordpress as of now 4.0!</strong>
+
+== Installation ==
+
+1. Upload `youtubevideo.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Manage your video list at 'Options -> Random YouTube Videos'
+4. Put the new widget into your sidebar
+
+== Screenshots ==
+
+1. This is the widget in your sidebar
+2. The page in your admin area
+3. Adds widget to sidebar and enables AutoPlay feature or not!
+As of 2.1 this is a new design.
+4. Now you can add more then 1 Random YouTube Widget!
+
+== Changelog ==
+* 1.0 - first release. Widget can be used, videos can be managed
+* 1.1 - some bugs fixed, e.g. new line after title
+* 1.2 - subfolder bug fixed, you can now use the plugin in a subfolder
+* 1.3 - Another bugfix. Add row button should work for everyone. Thanks to Cody
+* 1.3.5 - I hate bugs :P Add row button should definitively work now. Sorry ._.
+* 1.3.8 - I hate bugs AND I hate the Internet Explorer -.-" Apologies for the next bugfix update ._.
+* 1.5 - Now you can choose the width of the displayed video and (optionally) put a link to my blog below this video
+* 1.7 - Fixed language files and patched the broken youtube thanks to roycegracie! Also added the option to the youtube widget
+were the user can add an option to enable AutoPlay or not. Tested in Firefox and IE!
+* 1.8 - Fixed the widget now it is more modernized which means you can add as many widgets as you want to your sidebar! Community 
+suggestion! This has been tested in Firefox and IE and with the latest wordpress!
+* 1.9 - Fixed the widget now it is more modernized which means you can add as many widgets as you want to your sidebar! Community 
+suggestion! This has been tested in Firefox and IE and with the latest wordpress! I also made a new page for the widget to make it
+cleaner for any developers that like to add or delete stuff :)! No more error log!
+* 2.0 - Cleaned up the entire code now in DIVS no more tables. The admin page looks a lot different now view
+the screen shot on the plugin page. Fixed height so now you can set your own custom height instead of having
+it automatically set with the width. Removed screen shots that are not needed any more save more space for users.
+Also last but not least I added a preview video on the admin page as promised.
+* 2.1 - Added a new style sheet. So users could keep there changed colors during updates. Fixed most of the rare bugs. 
+Updated the YouTube player this has the latest iframe version. Removed auto width/height did not work that great on all 
+websites now you can choose in pixles! Updated some of the php code. This caused php errors and people who are hosted with 
+Windows could not activate the plugin. Added a checkbox for AutoPlay option instead of manually typing it in. This should
+work 100% on release tested with version 4.0 and up!
---
+++ youtubevideo.php
@@ -0,0 +1,151 @@
+<?php
+/*
+Plugin Name: Random YouTube Video
+Plugin URI: http://wordpress.org/plugins/random-youtube-video/
+Description: This widget shows a random youtube video from your video list in your wordpress sidebar
+Author: Shobba, roycegracie, zigvt85
+Author URI: http://wordpress.org/plugins/random-youtube-video/
+Version: 2.1
+License: GPL compatible
+*/
+
+/*  Copyright 2008  Marcel Eichhorn
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    For a copy of the GNU General Public License see <http://www.gnu.org/licenses/>.
+*/
+
+require_once("widget.php");
+
+function addrowfunc()
+{
+    $url = get_bloginfo('wpurl');
+    echo "\n\t<!-- Added By Random YouTube Video -->\n\t";
+    ?>
+	<script language="JavaScript">
+		function newrow(last, obj){
+			var latestid = last;
+			tb = document.getElementById("asd");
+			var inn2 = '<table width="100%"><tr><th><input type="Text" name="titel['+latestid+']" value="" size="50"></th><th><input type="Text" name="url['+latestid+']" value="" size="50"></th></tr></table>';
+			tb.innerHTML += inn2;
+			/*
+			tb = document.getElementById("greattable");
+			tr = document.createElement("tr");
+			th1 = document.createElement("th");
+			th2 = document.createElement("th");
+
+			tb.appendChild(tr);
+			tr.appendChild(th1);
+			tr.appendChild(th2);
+
+			th1.innerHTML = '<input type="Text" name="titel['+latestid+']" value="" size="50">';
+			th2.innerHTML = '<input type="Text" name="url['+latestid+']" value="" size="50">';*/
+			latestid += 1;
+			obj.onclick = function(){newrow(latestid, obj);};
+
+			return false;
+		}
+	</script>
+    <?php
+//    echo "\n\t<script language='text/javascript' src='".$url."/wp-content/plugins/randomyoutubevideo/addrow.js'></script>";
+}
+function ryv_adminpage(){
+	global $wpdb;
+	if($_POST['ryv_submit']){
+	$leeren = $wpdb->query("TRUNCATE TABLE `".$wpdb->prefix."randomyoutube`");
+		if($_POST['url']){
+			foreach($_POST['url'] as $key => $con){
+				if($con){
+					$ins = $wpdb->query("INSERT INTO `".$wpdb->prefix."randomyoutube` (`id`,`url`,`titel`) VALUES (".$key.",'".$con."','".$_POST['titel'][$key]."')");
+				}
+			}
+			$message = '<div class="updated"><p><strong>Saved.</strong></p></div>';
+		}
+	}?>
+	<div class="wrap">
+		<?=$message?>
+		<h2>Random YouTube Video Management</h2>
+		<form name="ryv" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+			<fieldset class="options">
+				<legend>YouTube Video List</legend>
+				<center>
+				To delete one entry just delete its url and click "Save"
+				<table width="80%" cellspacing="2" cellpadding="3" class="editform" id="greattable">
+					<tr class="videowrap">
+						<th style="text-align: center;">Video Title (Optional)</th>
+						<th style="text-align: center;">Video ID (From Embed Code)<br>(looks like: <span class="videoid"><a href="https://www.youtube.com/watch?v=096t3RyU0jY" target="_blank">096t3RyU0jY</a></span>)</th>
+					</tr>
+					<?php
+					$vids = $wpdb->get_results("SELECT * FROM `".$wpdb->prefix."randomyoutube` ORDER BY id");
+					foreach ($vids as $vid){
+						$letzte_id = $vid->id;
+						?>
+					<tr>
+						<th style="text-align: center;"><input type="Text" name="titel[<?=$vid->id?>]" value="<?=$vid->titel?>" size="50"></th>
+						<th style="text-align: center;"><input type="Text" name="url[<?=$vid->id?>]" value="<?=$vid->url?>" size="50"></th>
+					</tr>
+					<?php
+					}
+					?>
+				</table><table width="80%" cellspacing="2" cellpadding="3" class="editform"><tr><td id="asd"></td></tr></table>
+				<table width="80%" cellspacing="2" cellpadding="3" class="editform">
+					<tr>
+						<th colspan="2" style="text-align: left;"><input class="button" type="button" name="addrow_button" value="+ add row" onClick="return newrow(<?=$letzte_id+1?>, this);"></th>
+					</tr>
+				</table><input class="button" type="Submit" name="ryv_submit" value="Save"></center>
+			</fieldset>
+			<div style="text-align:center;">
+				<strong>Designed and Coded By:</strong> <a href="http://www.soslidesigns.com" target="_blank">So Sli Designs</a> - <strong>Orginal Coder:</strong> <a href="https://profiles.wordpress.org/shobba/" target="_blank">Shobba</a>
+				<br />
+				This plugin is now maintained by the community!
+			</div>
+		</form>
+	</div>
+	<?php
+}
+
+function ryv_adminmenu() {
+    add_submenu_page('options-general.php', 'Random YouTube Video &raquo; Manage Videos', 'Random Youtube Videos', 10, __FILE__, 'ryv_adminpage');
+}
+add_action('admin_menu', 'ryv_adminmenu');
+
+function ryv_install(){
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "randomyoutube";
+	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name){
+	$sql = "CREATE TABLE " . $table_name . " (
+	  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	  url TEXT NOT NULL,
+	  titel TEXT NOT NULL,
+	  autoplay TEXT NOT NULL,
+	  width TEXT NOT NULL,
+	  height TEXT NOT NULL,
+	  UNIQUE KEY id (id)
+	);";
+
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($sql);
+	}
+}
+    add_action( 'admin_enqueue_scripts', 'safely_add_stylesheet_to_admin' );
+
+    /*Add stylesheet to the page*/
+    function safely_add_stylesheet_to_admin() {
+        wp_enqueue_style( 'prefix-style', plugins_url('css/style.css', __FILE__) );
+    }
+
+register_activation_hook(__FILE__,'ryv_install');
+//if ($_REQUEST['page'] == "youtubevideo.php")
+    add_action('admin_head', 'addrowfunc');
+	
+?>
